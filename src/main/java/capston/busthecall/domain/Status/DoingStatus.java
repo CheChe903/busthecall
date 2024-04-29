92c2380f98de9ee928bb927b81a1a007cbefeced
KimJinYeong <cheche903@naver.com>
2024-04-29 13:09:35 +0900

    <<통합 작업>> 기존 소스파일 삭제

--- src/main/java/capston/busthecall/domain/Status/DoingStatus.java
+++
@@ -1,6 +0,0 @@
-package capston.busthecall.domain.Status;
-
-public enum DoingStatus {
-
-    BOARD,DROP
-}
--- src/main/java/capston/busthecall/repository/BusRepository.java
+++
@@ -1,7 +0,0 @@
-package capston.busthecall.repository;
-
-import capston.busthecall.domain.Bus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BusRepository extends JpaRepository<Bus, Long> {
-}
